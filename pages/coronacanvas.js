bdde4770e6acfb972b239f8a19d076883b6fd6b7
jzakotnik <jure@zakotnik.de>
2022-01-07 23:14:36 +0100

    initial setup of canvas

---
+++ pages/coronacanvas.js
@@ -0,0 +1,41 @@
+import CanvasDraw from "react-canvas-draw";
+
+export default function CoronaCanvas({ topref }) {
+  /*const canvasConfig = {
+    loadTimeOffset: 5,
+    lazyRadius: 30,
+    brushRadius: 6,
+    brushColor: "#444",
+    catenaryColor: "#0a0302",
+    gridColor: "rgba(150,150,150,0.17)",
+    hideGrid: false,
+    canvasWidth: 800,
+    canvasHeight: 400,
+    disabled: false,
+    imgSrc: "",
+    saveData: null,
+    immediateLoading: false,
+    hideInterface: false,
+    gridSizeX: 2,
+    gridSizeY: 25,
+    gridLineWidth: 0.5,
+    hideGridX: false,
+    hideGridY: false,
+    enablePanAndZoom: false,
+    mouseZoomFactor: 0.01,
+    zoomExtents: { min: 0.33, max: 3 },
+  };*/
+  console.log("Ref in CoronaCanvas: ", topref);
+  return (
+    <div>
+      <CanvasDraw
+        canvasWidth={900}
+        canvasHeight={600}
+        brushRadius={6}
+        gridSizeX={20}
+        ref={topref}
+        onChange={() => console.log("onChange")}
+      />
+    </div>
+  );
+}
